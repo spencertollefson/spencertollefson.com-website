6f7abc2d37f43076695c00b57076597517a713d2
Spencer Tollefson <spencer@spencertollefson.com>
2018-04-17 20:10:49 -0600

    remove secret_settings.py call from settings.py

--- website/settings.py
+++ website/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import dj_database_url
 import os
-from .secret_settings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
