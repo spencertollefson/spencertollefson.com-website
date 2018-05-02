a3574dc940f4b0fdce1fac0efb3f73151ea8eba3
Spencer Tollefson <spencer@spencertollefson.com>
2018-05-02 11:38:08 -0600

    Play with HTML, CSS, and navbar specifically

--- website/urls.py
+++ website/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('markdownx/', include('markdownx.urls')),
+    path('bootstrap/', TemplateView.as_view(template_name='bootstrap/example.html')),
+
 ]
