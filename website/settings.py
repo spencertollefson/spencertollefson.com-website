7089283cd668476d32749d7063f8b9d6c04026b8
Spencer Tollefson <spencer@spencertollefson.com>
2018-05-04 19:54:33 -0600

    Change back STATIC_URL setting. Add dummy static/blog/images/keep-image.txt file

--- website/settings.py
+++ website/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 
 
 # Extra places for collectstatic to find static files.
