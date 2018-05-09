9535d778acb59b2ee63a31cc65160f667a320809
Spencer Tollefson <spencer@spencertollefson.com>
2018-05-09 10:59:25 -0600

    Add Journal section to URLs and Views
    Still need to modify it, separate the entries to be different from Blog Posts, and possibly add pagination or simple scrolling

--- blog/urls.py
+++ blog/urls.py
@@ -2,12 +2,14 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('about/', views.about, name='about'),
-    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    # path('', views.post_list, name='post_list'),
+    path('', views.about, name='about'),
+    path('posts/', views.post_list, name='post_list'),
+    path('journal/', views.journal, name='journal'),
     path('markdownx/', include('markdownx.urls')),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('posts/<int:pk>/', views.post_detail, name='post_detail'),
     path('resume/', views.resume, name='resume'),
     path('sitemap.xml/', views.sitemap, name='sitemap'),
+    # path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    # path('drafts/', views.post_draft_list, name='post_draft_list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -10,6 +10,10 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+def journal(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, 'blog/journal_list.html', {'posts': posts})
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
