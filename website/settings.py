d5d292d6ee3070f3b0cd3c46478e1c56188778c4
Spencer Tollefson <spencer@spencertollefson.com>
2018-05-11 19:57:18 -0600

    Create 404.html page and test it works. Completely simple.

--- website/settings.py
+++ website/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
+# DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', '.spencertollefson.com']
 
