3d8219e114c000f498e6a897d9101d8990e44519
Spencer Tollefson <spencertollefson@gmail.com>
2021-03-19 15:08:37 -0400

    try one more model fix for website...

--- blog/api.py
+++ blog/api.py
@@ -9,7 +9,8 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import MultiLabelBinarizer, OneHotEncoder, StandardScaler
 
 # model = joblib.load(open('./web_app/stat_models/catboost_month_incident_20181206_updated_pkl_v_on_20210319.joblib', 'rb'))
-model = joblib.load(open('./web_app/stat_models/catboost_month_incident_12.06.2018.joblib', 'rb'))
+# model = joblib.load(open('./web_app/stat_models/catboost_month_incident_12.06.2018.joblib', 'rb'))
+model = joblib.load(open('./web_app/stat_models/catboost_month_incident_12.06.2018_v2.joblib', 'rb'))
 
 
 
--- web_app/api.py
+++ web_app/api.py
@@ -5,7 +5,8 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import MultiLabelBinarizer, OneHotEncoder, StandardScaler
 
 # from sklearn.externals import joblib
-model = joblib.load(open('./stat_models/catboost_month_incident_12.06.2018.joblib', 'rb'))
+# model = joblib.load(open('./stat_models/catboost_month_incident_12.06.2018.joblib', 'rb'))
+model = joblib.load(open('./stat_models/catboost_month_incident_12.06.2018_v2.joblib', 'rb'))
 # model = joblib.load(open('./stat_models/catboost_month_incident_20181206_updated_pkl_v_on_20210319.joblib', 'rb'))
 
 
